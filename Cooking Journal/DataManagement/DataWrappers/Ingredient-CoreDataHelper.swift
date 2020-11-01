ea7ea6dbfe3fab0657d8d6bb80207485d5049429
Szeplaki Daniel <danielszeplaki@icloud.com>
2020-11-01 19:43:14 +0100

    Wrapper for non-optional properties of classes.

---
+++ Cooking Journal/DataManagement/DataWrappers/Ingredient-CoreDataHelper.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe-CoreDataHelper.swift
+//  Cooking Journal
+//
+//  Created by Széplaki Dániel on 2020. 11. 01..
+//
+
+import Foundation
+
+extension Ingredient{
+    
+    var ingredientName : String{
+        name ?? ""
+    }
+}
